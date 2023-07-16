14240f43e15a192138f73e94f1a321445ea64f36
Manisha Johnson <manishajohnson@MacBook-Pro.local>
2023-07-17 00:30:47 +0530

    added whitenoise

--- vigilance/settings.py
+++ vigilance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,10 +132,9 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
