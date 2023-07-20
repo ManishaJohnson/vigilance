997f8c61b549ddf8e3999d21b0d061879df4ff32
Manisha Johnson <manishajohnson@MacBook-Pro.local>
2023-07-20 22:20:40 +0530

    remove commented lines

--- vigilance/settings.py
+++ vigilance/settings.py
@@ -83,13 +83,6 @@ WSGI_APPLICATION = 'vigilance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
